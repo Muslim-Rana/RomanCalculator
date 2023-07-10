56483b4e1b10c276a3cc6cd3ba93c9a07ea88059
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-07-10 16:47:18 -0400

    Began the implementation for converting a Roman numeral string into type integer in Roman.java

--- app/src/main/java/com/example/romancalculator/Roman.java
+++ app/src/main/java/com/example/romancalculator/Roman.java
@@ -1,4 +1,38 @@
 package com.example.romancalculator;
 
 public class Roman {
+
+    public static short findIndex (char letter, String[] romanLetters) {
+        for (short i = 0; i < romanLetters.length; i++) {
+            if (letter == romanLetters[i].charAt(0)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static short convertToInt (String romanNumber) throws RomanException {
+        //Case insensitivity
+        romanNumber = romanNumber.toUpperCase();
+
+        //Initial variables. romanLetters is mapped to romanValues.
+        String[] romanLetters = {"I", "V", "X", "L", "C", "D", "M"};
+        short[] romanValues = {1, 5, 10, 50, 100, 500, 1000};
+        short totalValue = 0;
+        short previousIndex = 0;
+        short sameValueCounter = 0;
+
+        //Reading the Roman numeral from right to left
+        for (int i = romanNumber.length() - 1; i >= 0; i--) {
+            char c = romanNumber.charAt(i);
+            short index = findIndex(c, romanLetters);
+
+            //If we get -1 from our findIndex method
+            if (index == -1) {
+                throw new RomanException(c + " is an invalid character!");
+            }
+        }
+
+        return 1; //temporary testing value
+    }
 }
