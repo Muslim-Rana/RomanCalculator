175c5c752ef727ddd97d165a8a7082a3a542b1e1
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-07-16 20:48:26 -0400

    Began writing MainActivity.java
    - Implemented a method called appendButtonValue which appends a given button's text to a TextView object

--- app/src/main/java/com/example/romancalculator/MainActivity.java
+++ app/src/main/java/com/example/romancalculator/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.romancalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //comment test for github commit
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.TextView1)).setText("");
+    }
+
+    /**
+     * Appends the button's text to the input text.
+     * @param view Used for accessing the button's text
+     */
+    public void appendButtonValue(View view) {
+        ((TextView) findViewById(R.id.TextView1)).setText(((TextView) findViewById(R.id.TextView1)).getText() + "" + ((TextView) findViewById(view.getId())).getText());
     }
 }
