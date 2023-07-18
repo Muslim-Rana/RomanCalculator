6cb8785cf509e8274c22a54d42bf277b66e9c99a
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-07-18 18:04:32 -0400

    Continued writing MainActivity.java
    - Began the implementation of a method called intToRoman which takes in a TextView object (input integer as a string), converts it to an integer and then to a Roman numeral.

--- app/src/main/java/com/example/romancalculator/MainActivity.java
+++ app/src/main/java/com/example/romancalculator/MainActivity.java
@@ -22,4 +22,21 @@ public class MainActivity extends AppCompatActivity {
     public void appendButtonValue(View view) {
         ((TextView) findViewById(R.id.TextView1)).setText(((TextView) findViewById(R.id.TextView1)).getText() + "" + ((TextView) findViewById(view.getId())).getText());
     }
+
+    /**
+     * Takes our input text field, converts it to an integer and then further converts it to a roman numeral
+     * @param view Used for accessing feautres of android studio like the button text
+     * @throws RomanException Catches invalid values
+     */
+    public void intToRoman(View view) throws RomanException {
+        Roman r = new Roman();
+        String s = ((TextView) findViewById(R.id.TextView1)).getText().toString();
+        TextView t = (TextView) findViewById(R.id.outText);
+        if (Integer.parseInt(s) <= 0 || Integer.parseInt(s) >= 5000) {
+            t.setText("INVALID VALUE! OUT OF BOUNDS!");
+        }
+
+        //todo
+    }
+
 }
