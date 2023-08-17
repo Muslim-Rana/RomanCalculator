c62f2f68ddfd6afcad0b462ca45e7acddf79003c
Muslim-Rana <muslim.inaam.rana@gmail.com>
2023-08-16 23:39:29 -0400

    Updated RomToInt.java
    - Implemented the same appendButtonValue and clearTexts methods as were used for the integer to roman screen of the app.

--- app/src/main/java/com/example/romancalculator/RomToInt.java
+++ app/src/main/java/com/example/romancalculator/RomToInt.java
@@ -1,6 +1,7 @@
 package com.example.romancalculator;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,4 +14,22 @@ public class RomToInt extends AppCompatActivity {
         setContentView(R.layout.activity_rom_to_int);
         ((TextView) findViewById(R.id.inTextA2)).setText("");
     }
+
+    /**
+     * Appends the button's text to the input text.
+     * @param view Used for accessing the button's text
+     */
+    public void appendButtonValue(View view) {
+        ((TextView) findViewById(R.id.inTextA2)).setText(((TextView) findViewById(R.id.inTextA2)).getText() + "" + ((TextView) findViewById(view.getId())).getText());
+    }
+
+    /**
+     * Clears both the input and output text fields
+     * @param view Used for accessing the text fields
+     */
+    public void clearTexts (View view) {
+        ((TextView) findViewById(R.id.inTextA2)).setText("");
+        ((TextView) findViewById(R.id.outTextA2)).setText("");
+    }
+
 }
